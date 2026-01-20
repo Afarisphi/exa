84ec31fc5e3f8acecfc126150fdfbe92070d0bd7
Afarisphi <agung.afr10@gmail.com>
2026-01-20 10:11:02 +0700

    vercel

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import attendanceRoutes from './attendance.routes.js';
 
 const router = express.Router();
 
+router.get('/', (_, res) => {
+  res.json({ status: 'ok' });
+});
+
 router.use('/attendance', attendanceRoutes);
 
 router.get('/health', (req, res) => {
